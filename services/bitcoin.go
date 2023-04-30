8a15eaee1c434351d74597cc366a326c1f1dcb0d
Houdini <nonsoamadi@aol.com>
2023-04-30 10:04:56 +0100

    chore: add readme and license

--- services/bitcoin.go
+++ services/bitcoin.go
@@ -2,8 +2,10 @@ package services
 
 import (
 	"log"
+	"math"
 
 	"github.com/NonsoAmadi10/p2p-analysis/bitcoin"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 )
 
 type NodeMetrics struct {
@@ -13,6 +15,7 @@ type NodeMetrics struct {
 	Blocks        int32       `json:"no_of_blocks"`
 	BestBlockHash string      `json:"bestblockhash"`
 	UserAgent     interface{} `json:"user_agent"`
+	HashRate      float64     `json:"hash_rate"`
 }
 
 func GetInfo() *NodeMetrics {
@@ -29,6 +32,19 @@ func GetInfo() *NodeMetrics {
 
 	networkInfo, _ := client.GetNetworkInfo()
 
+	lastBlockHash, err := chainhash.NewHashFromStr(info.BestBlockHash)
+	if err != nil {
+		log.Println(err)
+	}
+
+	lastBlock, err := client.GetBlock(lastBlockHash)
+	if err != nil {
+		log.Println(err)
+	}
+
+	timeToFindBlock := lastBlock.Header.Timestamp.Unix() - int64(lastBlock.Header.PrevBlock[len(lastBlock.Header.PrevBlock)-1])
+	hashrate := float64(info.Difficulty) / (float64(timeToFindBlock) * math.Pow(2, 32))
+
 	metrics := &NodeMetrics{
 		Difficulty:    info.Difficulty,
 		Version:       networkInfo.Version,
@@ -36,6 +52,7 @@ func GetInfo() *NodeMetrics {
 		Blocks:        info.Blocks,
 		BestBlockHash: info.BestBlockHash,
 		UserAgent:     networkInfo.SubVersion,
+		HashRate:      hashrate,
 	}
 
 	return metrics
